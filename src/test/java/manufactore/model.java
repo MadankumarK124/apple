3316a6f74175dc1d0c1c4b24cfc214b22d36807b
MADAN KUMAR K <MADAN KUMAR K@LAPTOP-IQ9B34OU>
2022-11-22 19:17:43 +0530

    macbook

--- src/test/java/manufactore/model.java
+++ src/test/java/manufactore/model.java
@@ -3,7 +3,10 @@ package manufactore;
 public class model {
 	public static void main(String[] args) {
 		System.out.println("macbook-pro");
-		System.out.println("macbook-air");
+		System.out.println("macbook-air  ");
+		System.out.println("macbook-studio  ");
+		
+		
 		
 	}
 }
